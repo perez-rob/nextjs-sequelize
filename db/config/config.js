a5ac9dda75b582a2652abd60754b211513f7b0b7
bob.w.perez <bob.w.perez@gmail.com>
2021-08-05 14:58:36 -0400

    ok

--- db/config/config.js
+++ db/config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: 'root',
-    password: '',
-    database: 'bbtest',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     dialectOptions: {
       ssl: true,
--- db/models/index.js
+++ db/models/index.js
@@ -12,14 +12,22 @@ const db = {};
 const models = process.cwd() + '/db/models/' || __dirname;
 
 let sequelize;
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
     config,
+    {
+      pool: {
+        max: 5,
+        min: 0,
+        acuire: 30000,
+        idle: 10000,
+      },
+    },
   );
 }
 /* fs.readdirSync(__dirname) */
