9e7f9a44eacb77cc2bed5fe8b5a35c415a3ababd
bob.w.perez <bob.w.perez@gmail.com>
2021-08-05 11:44:15 -0400

    ok2

--- db/config/config.js
+++ db/config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: 'root',
+    password: '',
+    database: 'bbtest',
+    host: '127.0.0.1',
     dialect: 'mysql',
     dialectOptions: {
       ssl: true,
---
+++ pscale.js
@@ -0,0 +1,5 @@
+export const PLANETSCALE_TOKEN =
+  'pscale_tkn_b59QvQDW9o_Jba9sKEoPJ8Vru5TjQ_kNBBXplO89VDw';
+export const PLANETSCALE_TOKEN_NAME = 'lqa2kypxedbo';
+export const PLANETSCALE_ORG = '[YOUR ORG]';
+export const PLANETSCALE_DB = 'bbtest';
